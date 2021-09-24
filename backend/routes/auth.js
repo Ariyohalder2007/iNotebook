3edd408b102bbafa7987d5a18bce6fefec7e9a76
ariyohalder <ariyohalder@gmail.com>
2021-09-24 20:23:51 +0530

    done login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,4 +49,46 @@ router.post(
   }
 );
 
+
+//Auth a user using POST: "/api/auth/login".  Doesn't require login.
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password Cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try{
+      const {email, password}=req.body;
+      let user = await User.findOne({email})
+      if (!user) {
+        return res.status(400).json({error:"Use Correct Credentials, And we're not gonna give you any info about LOGIN!"})
+
+      }
+      const passwordCompare=await bcrypt.compare(password, user.password)
+      if (!passwordCompare) {
+        return res.status(400).json({error:"Use Correct Credentials, And we're not gonna give you any info about LOGIN!"})
+      }
+      const data={
+        user:{
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json ({authToken});
+      }catch(error){
+        res.status(500).send("Some error occured, Log: "+error.message);
+        console.error(error.message);
+      }
+
+
+  })
+
+
+
+
 module.exports = router;
